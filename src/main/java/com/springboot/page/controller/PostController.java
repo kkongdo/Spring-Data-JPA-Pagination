5c39e0610e609a4c6b72d6286fdc211fae61c331
kkongdo <kkongdoes@naver.com>
2024-06-30 21:58:24 +0900

    chore: ãPaging 인ì처리에서 사용하는 인자 설명

--- src/main/java/com/springboot/page/controller/PostController.java
+++ src/main/java/com/springboot/page/controller/PostController.java
@@ -22,10 +22,12 @@ public class PostController {
     public ResponseEntity<List<PostResponseDto>> getPost(@RequestParam(required = false) Integer page) {
         List<PostResponseDto> postResponseDtoList;
         if (page == null) {
-            postResponseDtoList = postService.findAll();
+            postResponseDtoList = postService.findAll(); // 페이지가 없는 전체 목록을 출력할 때
         } else {
-            int size = 3;
+            // 페이징을 원하는 목록 출력
+            int size = 3; // 페이지 하나에 가져올 객체의 수 : 여기서는 3개
             Pageable pageable = PageRequest.of(page - 1, size, Sort.Direction.ASC, "title");
+            // 첫번째 인자는 시작 페이지 : 1부터 시작할려면 -1을 해줌, 두번째 인자는 size, 세번째 인자는 정렬(오름차순or내림차순), 네번째 인자는 정렬 기준
             postResponseDtoList = postService.findAllPaging(pageable);
         }
         return postResponseDtoList != null && !postResponseDtoList.isEmpty() ?
