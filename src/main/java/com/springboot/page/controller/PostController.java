aea312ad67e1e24d8c772d9fa0fdda948c848bf5
kkongdo <kkongdoes@naver.com>
2024-07-21 23:38:00 +0900

    refactor: Controller와 Service refactor 완료

--- src/main/java/com/springboot/page/controller/PostController.java
+++ src/main/java/com/springboot/page/controller/PostController.java
@@ -1,16 +1,13 @@
 package com.springboot.page.controller;
 
 import com.springboot.page.common.response.BaseResponse;
-import com.springboot.page.common.response.BaseResponseStatus;
 import com.springboot.page.dto.PostDto;
 import com.springboot.page.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,25 +18,9 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts")
-    public BaseResponse<List<PostDto.PostResponseDto>> getPost(@RequestParam(required = false) Integer page) {
-        List<PostDto.PostResponseDto> postResponseDtoList;
-        if (page == null) {
-            postResponseDtoList = postService.findAll(); // 페이지가 없는 전체 목록을 출력할 때
-        } else {
-            // 페이징을 원하는 목록 출력
-            int size = 3; // 페이지 하나에 가져올 객체의 수 : 여기서는 3개
-            Pageable pageable = PageRequest.of(page - 1, size, Sort.Direction.ASC, "title");
-            // 첫번째 인자는 시작 페이지 : 1부터 시작할려면 -1을 해줌,
-            // 두번째 인자는 size, 세번째 인자는 정렬(오름차순or내림차순),
-            // 네번째 인자는 정렬 기준
-            postResponseDtoList = postService.findAllPaging(pageable);
-        }
-//        return postResponseDtoList != null && !postResponseDtoList.isEmpty() ?
-//                ResponseEntity.status(HttpStatus.OK).body(postResponseDtoList) :
-//                ResponseEntity.status((HttpStatus.NOT_FOUND)).build();
-
+    public BaseResponse<List<PostDto.PostResponseDto>> getPost(Pageable pageable) {
+        List<PostDto.PostResponseDto> postResponseDtoList = postService.findAll(pageable);
         // CustomResponse를 이용하여 응답 전달
-        return postResponseDtoList != null && !postResponseDtoList.isEmpty() ?
-                new BaseResponse<>(postResponseDtoList) : new BaseResponse<>();
+        return new BaseResponse<>(postResponseDtoList);
     }
 }
--- src/main/java/com/springboot/page/service/PostService.java
+++ src/main/java/com/springboot/page/service/PostService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,15 +20,11 @@ public class PostService {
 
     private final PostRepository postRepository;
 
-    public List<PostDto.PostResponseDto> findAllPaging(Pageable pageable) {
-        Page<Post> postsPages = postRepository.findAll(pageable);
-        List<PostDto.PostResponseDto> postResponseDtoPages = postsPages.map(PostDto.PostResponseDto::from).getContent();
-        return postResponseDtoPages;
-    }
-
-    public List<PostDto.PostResponseDto> findAll() {
-        List<Post> postList = postRepository.findAll();
-        List<PostDto.PostResponseDto> responseDtoList = postList.stream().map(PostDto.PostResponseDto::from).collect(Collectors.toList());
-        return responseDtoList;
+    public List<PostDto.PostResponseDto> findAll(Pageable pageable) {
+        // 페이징을 원하는 목록 출력, 첫번째 인자는 시작 페이지 : 1부터 시작할려면 -1을 해줌, 두번째 인자는 size, 세번째 인자는 정렬(오름차순or내림차순), 네번째 인자는 정렬 기준
+        int size = 3; // 페이지 하나에 가져올 객체의 수 : 여기서는 3개
+        Pageable page = PageRequest.of(pageable.getPageNumber() - 1, size, Sort.Direction.ASC, "title");
+        Page<Post> postsPageList = postRepository.findAll(page);
+        return postsPageList.map(PostDto.PostResponseDto::from).getContent();
     }
 }
